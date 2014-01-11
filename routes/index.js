0ea582f0eb029b41bc5d919b0f607f6ab667909e
James H. Edwards <james.howe.edwards@gmail.com>
2014-01-11 16:49:14 +0800

    add search algorithm

--- routes/index.js
+++ routes/index.js
@@ -35,16 +35,70 @@ exports.create  = function ( req, res ) {
 			})
 	}
 exports.search = function ( req, res ) {
-  var query = req.body.searchQuery;
-  poem.find({title: query}).exec(function (err, poem){
-    res.render('index', {
-      title: 'Poetry Link',
-      poems: poem
-    });
+  poem.find( function (err, poems){
+    var i;
+    var k;
+    var titles = [];
+    var results = [];
+    for(i=0;i<poems.length;i++){
+      titles.push(poems[i].title);
+    }
+    var query = req.body.searchQuery.toLowerCase();
+    var matches = match(query, titles);
+    console.log(matches);
+    for(i=0;i<matches.length;i++){
+      for(k=0;k<poems.length;k++){
+        if(poems[k].title === matches[i]){
+          results.push(poems[k]);
+        }
+      }
+    };
+  res.render('index', {
+    title: 'Poetry Link',
+    poems: results
+    })
   });
 }
 
 exports.author = function(req, res){
 	
 }
+
+var check = function( quer, title ) {
+  var x = 0;
+  var y = quer.length;
+  for( i=0; i<quer.length; i++) {
+    if( quer[i] === title[i] ){
+      x += 1;
+    }
+  }
+  if( x >= y ) { 
+    return true;
+  } else {
+    return false;
+  }
+};
+
+var match = function ( string, array ) {
+  var i;
+  var k;
+  var results = [];
+  for(i=0;i < array.length; i++) {
+    var current = array[i].split(" ");
+    if( current.length > 1 ) {       
+      for(k=0;k<current.length;k++) {
+        if(check( string, current[k].toLowerCase())) {
+          results.push( array[i] ); 
+          break; 
+        }    
+      }
+    }   
+    else {
+      if(check( string, array[i].toLowerCase())) {
+        results.push(array[i]);
+      }
+    }
+  }
+  return results;
+};
 //callbacks can be nested indifiniely such that the res.render method has access to all the variables hoobly!!!
